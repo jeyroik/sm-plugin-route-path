9b318a17ecf910a1eab93f846caeece3d7a09eb4
Aleksandr Ivanov <aivanov@hq.fix.ru>
2018-06-12 14:27:57 +0300

    init

---
+++ src/components/systems/states/plugins/PluginRouteToPath.php
@@ -0,0 +1,38 @@
+<?php
+namespace tratabor\components\systems\states\plugins;
+
+use tratabor\components\systems\Plugin;
+use tratabor\interfaces\systems\states\IStatesRoute;
+use tratabor\interfaces\systems\states\plugins\IPluginRouteTo;
+
+/**
+ * Class PluginRouteToPath
+ *
+ * @package tratabor\components\systems\states\plugins
+ * @author Funcraft <me@funcraft.ru>
+ */
+class PluginRouteToPath extends Plugin implements IPluginRouteTo
+{
+    const ROUTE__PATH = '@directive.stateRoute().__path()';
+
+    /**
+     * @param IStatesRoute $route
+     * @param $toStateId
+     *
+     * @return string
+     */
+    public function __invoke(IStatesRoute &$route, $toStateId)
+    {
+        $routeContent = $route->getRoute();
+
+        if (!isset($routeContent[static::ROUTE__PATH])) {
+            $routeContent[static::ROUTE__PATH] = [];
+        }
+
+        $routeContent[] = [$route->getCurrentFrom() => $toStateId];
+
+        $route->setRoute($routeContent);
+
+        return $toStateId;
+    }
+}
